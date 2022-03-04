14a4b64f4536d886aceb93ef782014b5af2d8ecc
hustchihieu143 <chihieusky@gmail.com>
2022-03-05 00:24:18 +0700

    init project

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { databaseConfig } from 'src/configs/database.config';
+import { Modules } from './modules';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(databaseConfig)],
+  imports: [TypeOrmModule.forRoot(databaseConfig), ...Modules],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/modules.ts
@@ -0,0 +1,4 @@
+import { UserController } from './modules/user/user.controller';
+
+export const Modules = [UserController];
+export default Modules;
